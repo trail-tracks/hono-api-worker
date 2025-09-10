0b97e3e53dde014f16e64746db5ef93aaa3013bf
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-09-09 22:06:17 -0300

    feat: add password column to users table and update schema

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -4,4 +4,5 @@ export const users = sqliteTable('users', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   name: text('name').notNull(),
   email: text('email').unique().notNull(),
+  password: text('password').notNull(),
 });
