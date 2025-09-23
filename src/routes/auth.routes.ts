970f72730f03a36a509fc0614ac36e7da5ecdf8e
EmanuelCAC <emanuelc279@gmail.com>
2025-09-22 21:21:35 -0300

    patch

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,75 +1,53 @@
-import { Hono } from 'hono';
-import { zValidator } from '@hono/zod-validator';
-import { hash } from 'bcryptjs';
-import { editSchema } from '../dtos/edit.dto';
-import { EditEntityController } from '../controllers/edit-entity.controller';
-import { LoginController } from '../controllers/login.controller';
-import { loginSchema } from '../dtos/login.dto';
-import { getDb } from '../../drizzle/db';
-import { entity } from '../../drizzle/schema';
-import { authMiddleware } from '../middlewares/auth-middleware';
-import { CreateEntityDto } from '../dtos/signup.dto';
-import { EntitiesController } from '../controllers/signup-entity.controller';
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import { hash } from "bcryptjs";
+import { editSchema } from "../dtos/edit.dto";
+import { EditEntityController } from "../controllers/edit-entity.controller";
+import { LoginController } from "../controllers/login.controller";
+import { loginSchema } from "../dtos/login.dto";
+import { getDb } from "../../drizzle/db";
+import { entity } from "../../drizzle/schema";
+import { authMiddleware } from "../middlewares/auth-middleware";
+import { CreateEntityDto } from "../dtos/signup.dto";
+import { EntitiesController } from "../controllers/signup-entity.controller";
 
-type Env = { DB: D1Database };
+type Env = {
+  Bindings: {
+    DB: D1Database;
+    JWT_SECRET: string;
+  };
+};
+
 type Variables = {
   jwtPayload: {
     userId: string;
   };
 };
 
-const authRoutes = new Hono<{ Bindings: Env, Variables: Variables }>();
+const authRoutes = new Hono<{ Bindings: Env; Variables: Variables }>();
 const editEntityController = new EditEntityController();
+const entitiesController = new EntitiesController();
 
 authRoutes.put(
-  '/edit',
+  "/edit",
   authMiddleware,
-  zValidator('json', editSchema),
-  editEntityController.edit.bind(editEntityController),
+  zValidator("json", editSchema),
+  editEntityController.edit.bind(editEntityController)
 );
 
 const loginController = new LoginController();
 
 authRoutes.post(
-  '/login',
-  zValidator('json', loginSchema),
-  loginController.handle.bind(loginController),
+  "/login",
+  zValidator("json", loginSchema),
+  loginController.handle.bind(loginController)
 );
 
 // Rota de registro (signup)
 authRoutes.post(
-  '/signup',
-  zValidator('json', CreateEntityDto),
-  (c) => EntitiesController.create(c)
+  "/signup",
+  zValidator("json", CreateEntityDto),
+  entitiesController.create.bind(entitiesController)
 );
-
-authRoutes.post('/register-entity', async (c) => {
-  try {
-    const data = await c.req.json();
-
-    // Hash da senha
-    const hashedPassword = await hash(data.password, 10);
-
-    // Insere a nova entidade no banco
-    await getDb(c.env.DB).insert(entity).values({
-      name: data.name,
-      nameComplement: data.nameComplement || null,
-      email: data.email,
-      password: hashedPassword,
-      zipCode: data.zipCode,
-      address: data.address,
-      number: data.number,
-      city: data.city,
-      state: data.state,
-      addressComplement: data.addressComplement || null,
-      phone: data.phone,
-    }).run();
-
-    return c.json({ message: 'Entity registered successfully' }, 201);
-  } catch (error) {
-    console.error(error);
-    return c.json({ error: 'Internal server error' }, 500);
-  }
-});
 
 export default authRoutes;
