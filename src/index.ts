248f2948cbcd149a30a51c540ec4e6b0093f8374
lukascampos <lukas.campos@aluno.ifsp.edu.br>
2025-09-23 21:37:10 -0300

    Add CORS support and JWT secret sample configuration

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,21 @@ type Variables = {
 
 const app = new Hono<{ Bindings: Env, Variables: Variables }>();
 
+app.use('*', async (c, next) => {
+  c.header('Access-Control-Allow-Origin', 'https://trilhainterativa.com.br');
+  c.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  c.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  c.header('Access-Control-Allow-Credentials', 'true');
+
+  // Responde às requisições OPTIONS diretamente
+  if (c.req.method === 'OPTIONS') {
+    return new Response('', { status: 204 });
+  }
+
+  await next();
+  return c.res;
+});
+
 app.get('/', authMiddleware, (c) => c.text('Hello Hono!'));
 
 app.route('/auth', authRoutes);
