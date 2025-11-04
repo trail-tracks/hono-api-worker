a8b5c950064ea067ceca9ab520cf501dc80ebad0
EmanuelCAC <emanuelc279@gmail.com>
2025-11-03 21:34:44 -0300

    Refactor entities routes to remove authentication middleware and streamline route handling

--- src/routes/entities.routes.ts
+++ src/routes/entities.routes.ts
@@ -1,15 +1,13 @@
 import { Hono } from 'hono';
 import { ListEntitiesController } from '../controllers/list-entities.controller';
 import { AppBindings, AppVariables } from '../types/env';
-import { authMiddleware } from '../middlewares/auth-middleware';
 
-const authRoutes = new Hono<{ Bindings: AppBindings; Variables: AppVariables }>();
+const entitiesRoutes = new Hono<{ Bindings: AppBindings; Variables: AppVariables }>();
 const listEntitiesController = new ListEntitiesController();
 
-authRoutes.get(
+entitiesRoutes.get(
   '/',
-  authMiddleware,
   listEntitiesController.list.bind(listEntitiesController),
 );
 
-export default authRoutes;
+export default entitiesRoutes;
